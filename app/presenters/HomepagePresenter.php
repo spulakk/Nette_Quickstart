e587fb89f2a38ae1d6b61f7bd060c142d969e811
spulakk <spulak.k@seznam.cz>
2018-02-22 01:50:44 +0100

    home-page section done

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -7,4 +7,17 @@ use Nette;
 
 class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault()
+    {
+        $this->template->posts = $this->database->table('posts')
+            ->order('created_at DESC')
+            ->limit(5);
+    }
 }
