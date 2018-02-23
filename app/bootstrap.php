6376f65bc353119151f77bfd15c1567bd564460e
spulakk <spulak.k@seznam.cz>
2018-02-23 16:04:30 +0100

    comment error

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,7 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode(false); // enable for your remote IP
+$configurator->setDebugMode(true); // enable for your remote IP
 $configurator->enableTracy(__DIR__ . '/../log');
 
 $configurator->setTimeZone('Europe/Prague');
---
+++ app/presenters/templates/PostPresenter.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+class PostPresenter extends Nette\Application\UI\Presenter
+{
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderShow($postId)
+    {
+        $post = $this->database->table('posts')->get($postId);
+        if (!$post) {
+            $this->error('Stránka nebyla nalezena');
+        }
+
+        $this->template->post = $post;
+        $this->template->comments = $post->related('comment')
+            ->order('created_at');
+    }
+
+    protected function createComponentCommentForm()
+    {
+        $form = new Form; //means Nette\Application\UI\Form
+
+        $form->addText('name', 'Jméno:')->setRequired();
+
+        $form->addEmail('email', 'Email:');
+
+        $form->addTextArea('comment', 'Komentář:')->setRequired();
+
+        $form->addSubmit('send','Publikovat komentář');
+
+        $form->onSuccess[] = [$this, 'commentFormSucceeded'];
+
+        return $form;
+    }
+
+    public function commentFormSucceeded($form, $values)
+    {
+        $postId = $this->getParameter('postId');
+
+        $this->database->table('comments')->insert([
+            'post_ID' => $postId,
+            'name' => $values->name,
+            'email' => $values->email,
+            'content' => $values->content,
+        ]);
+
+        $this->flashMessage('Děkuji za komentář', 'success');
+        $this->redirect('this');
+    }
+}
